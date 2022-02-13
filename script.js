07c34eb8be09e428ced3b7dbcc5344590843ebbd
Elizabeth Dowling <dowlinge94@gmail.com>
2022-02-13 12:18:11 -0500

    working JavaScript button FINALLY

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "script.js run test";
 document.querySelector("body").appendChild(h2);
+
+function clickyButton() {
+    alert("Thank you for visiting, feel free to email feedback using the link in my contact info!");
+  }
