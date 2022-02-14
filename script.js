8fb5133e59018eb48c820a4832f1b40909dc3543
Elizabeth Dowling <dowlinge94@gmail.com>
2022-02-14 12:06:56 -0500

    css intial check, some bugs fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "script.js run test";
-document.querySelector("body").appendChild(h2);
+const h3 = document.createElement("h3");
+h3.textContent = "script.js run test";
+document.querySelector("body").appendChild(h3);
 
 function clickyButton() {
     alert("Thank you for visiting, feel free to email feedback using the link in my contact info!");
